5d6d68e2c93d9b5e63979aaee922a91c07fe953a
Im Sunghoon <shi510.kor@gmail.com>
2019-08-13 19:39:27 +0900

    change feature name from eng to kor.

--- preprocessing/convert.py
+++ preprocessing/convert.py
@@ -7,63 +7,72 @@ def cvt_date(date):
     m = float(sp[0].split('-')[1])
     d = float(sp[0].split('-')[2])
     h = float(sp[1].split(':')[0])
-    return {'월': m, '일': d, '시': h}
+    return {'month': m, 'day': d, 'hour': h}
 
 def cvt_temperature(tp):
-    name = '기온'
+    name = 'temperature'
     if '' == tp:
         return {name: 0.0}
     else:
         return {name: float(tp)}
 
 def cvt_wind_speed(ws):
-    name = '풍속'
+    name = 'wind_speed'
     if '' == ws:
         return {name: 0.0}
     else:
         return {name: float(ws)}
 
 def cvt_relative_humidity(rh):
-    name = '습도'
+    name = 'humidity'
     if '' == rh:
         return {name: 0.0}
     else:
         return {name: float(rh)}
 
 def cvt_cloud_cover(cc):
-    name = '운량'
+    name = 'cloud_cover'
     if '' == cc:
         return {name: 0.0}
     else:
         return {name: float(cc)}
 
+def cvt_precipitation(pc):
+    name = 'precipitation'
+    if '' == pc:
+        return {name: 0.0}
+    else:
+        return {name: float(pc)}
+
 def cvt_radiation(rad):
-    name = '일사량'
+    name = 'radiation'
     if '' == rad:
         return {name: 0.0}
     else:
         return {name: float(rad)}
 
 norm_lambdas = {
-    '월': lambda x: x/12,
-    '일': lambda x: x/31,
-    '시': lambda x: x/23,
-    '기온': lambda x: (x+12.3)/(37.1+12.3),
-    '풍속': lambda x: x/16.3,
-    '습도': lambda x: x/100,
-    '운량': lambda x: x/10,
-    '일사량': lambda x: x/4.6
+    'month': lambda x: x/12,
+    'day': lambda x: x/31,
+    'hour': lambda x: x/23,
+    'temperature': lambda x: (x+12.3)/(37.1+12.3),
+    'wind_speed': lambda x: x/16.3,
+    'humidity': lambda x: x/100,
+    'cloud_cover': lambda x: x/10,
+    'precipitation': lambda x: x/73.5,
+    'radiation': lambda x: x/4.6
 }
 
 past_pair = {
     'input': {
-        '월': False,
-        '일': False,
-        '시': False,
-        '기온': True, 
-        '풍속': True,
-        '습도': True,
-        '운량': True
+        'month': False,
+        'day': False,
+        'hour': False,
+        'temperature': True, 
+        'wind_speed': True,
+        'humidity': True,
+        'cloud_cover': True,
+        'precipitation': True
     },
-    'output': '일사량'
+    'output': 'radiation'
 }
