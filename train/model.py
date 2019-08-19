6a167fefca0f37c8996fd69afe27f463081cd234
Im Sunghoon <shi510.kor@gmail.com>
2019-08-19 10:21:32 +0900

    modify models.

--- train/model.py
+++ train/model.py
@@ -3,16 +3,18 @@ import tensorflow.keras.layers as layers
 
 def model1(num_features):
     input = tf.keras.Input([num_features])
-    output = layers.Dense(256, layers.LeakyReLU())(input)
-    output = layers.Dense(64, layers.LeakyReLU())(output)
-    output = layers.Dense(1)(output)
+    output = layers.Dense(256)(input)
+    output = layers.LeakyReLU()(output)
+    output = layers.Dense(64)(output)
+    output = layers.LeakyReLU()(output)
+    output = layers.Dense(1, activation='sigmoid')(output)
     return tf.keras.Model(input, output)
 
 def model2(num_features):
     input = tf.keras.Input([num_features])
     output = _dense_bn_act(input, 256, layers.LeakyReLU())
     output = _dense_bn_act(output, 64, layers.LeakyReLU())
-    output = layers.Dense(1)(output)
+    output = layers.Dense(1, activation='sigmoid')(output)
     return tf.keras.Model(input, output)
 
 def _dense_bn_act(input, units, act):
@@ -24,22 +26,33 @@ def _dense_bn_act(input, units, act):
 def model3(num_features):
     input = tf.keras.Input((num_features))
     output = layers.Reshape((num_features, 1))(input)
-    output = _inception(output, 10)
-    output = _res_inception(output, 10)
-    output = _res_inception(output, 10)
+    output = _inception(output, 12)
+    output = _res_inception(output, 12)
+    output = _res_inception(output, 12)
     output = layers.Flatten()(output)
-    output = layers.Dense(128, activation=layers.LeakyReLU())(output)
-    output = layers.Dropout(0.5)(output)
-    output = layers.Dense(32, activation=layers.LeakyReLU())(output)
-    output = layers.Dropout(0.1)(output)
-    output = layers.Dense(1)(output)
+    output = layers.Dense(128)(output)
+    output = layers.LeakyReLU()(output)
+    output = layers.BatchNormalization()(output)
+    output = layers.Dense(64)(output)
+    output = layers.LeakyReLU()(output)
+    output = layers.BatchNormalization()(output)
+    output = layers.Dense(1, activation='sigmoid')(output)
     return tf.keras.Model(input, output)
 
 def _inception(x, filters):
     act = layers.LeakyReLU()
-    c3 = layers.Conv1D(filters, 3, padding='same', activation=act)(x)
-    c5 = layers.Conv1D(filters, 5, padding='same', activation=act)(x)
-    c7 = layers.Conv1D(filters, 7, padding='same', activation=act)(x)
+    c3 = layers.Conv1D(filters, 3, padding='same')(x)
+    c3 = layers.BatchNormalization()(c3)
+    c3 = layers.LeakyReLU()(c3)
+
+    c5 = layers.Conv1D(filters, 5, padding='same')(x)
+    c5 = layers.BatchNormalization()(c5)
+    c5 = layers.LeakyReLU()(c5)
+
+    c7 = layers.Conv1D(filters, 7, padding='same')(x)
+    c7 = layers.BatchNormalization()(c7)
+    c7 = layers.LeakyReLU()(c7)
+
     output = layers.Concatenate()([c3, c5, c7])
     return output
 
