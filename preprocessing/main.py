8d5a75b2f3d18789264aad2a4c091022d9af48a5
Im Sunghoon <shi510.kor@gmail.com>
2019-10-17 15:24:50 +0900

    add transforming from csv to tfrecord.

--- preprocessing/main.py
+++ preprocessing/main.py
@@ -3,27 +3,46 @@ import math
 import os
 import numpy as np
 import common.util as cutil
-import preprocessing.util as putil
 import preprocessing.statistic as statistic
 import preprocessing.convert as convert
+import tensorflow as tf
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--config_file')
+parser.add_argument('-cfg')
 parser.add_argument('--print_statistics', type=bool)
+parser.add_argument('tfrecord')
+
+def _tf_float_list(value):
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+
+def make_tfrecord(file, interestings, future_hour, past_hour, past_pair):
+    dict_list = cutil.convert_csv2dict_list(file, interestings)
+    dict_list = statistic.normalize(dict_list, convert.norm_lambdas)
+    x, y = cutil.make_past_pair(
+        dict_list, future_hour, past_hour, past_pair)
+    file_name = os.path.splitext(os.path.basename(file))[0]
+    tf_file = tf.io.TFRecordWriter(file_name+'.tfrecord')
+    for a, b in zip(x, y):
+        features = []
+        for name in a:
+            features.append(a[name])
+        feature = {
+            'features': _tf_float_list(features),
+            'radiation': _tf_float_list([b])
+        }
+        exam = tf.train.Example(features=tf.train.Features(feature=feature))
+        tf_file.write(exam.SerializeToString())
+    tf_file.close()
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    cfg = cutil.open_config_file(args.config_file)
 
+    if args.tfrecord:
+        cfg = cutil.open_config_file(args.cfg)
+        for file in cfg['files']:
+            make_tfrecord(file, cfg['interestings'], 1, cfg['past_hour'], convert.past_pair)
     if args.print_statistics:
         for file in cfg['files']:
             print('File: {}'.format(file))
             dict_list = cutil.convert_csv2dict_list(file, cfg['interestings'])
             statistic.print_statistics(dict_list)
-    else:
-        for file in cfg['files']:
-            dict_list = cutil.convert_csv2dict_list(file, cfg['interestings'])
-            dict_list = statistic.normalize(dict_list, convert.norm_lambdas)
-            x, y = cutil.make_past_pair(dict_list, cfg['past_hour'], convert.past_pair)
-            file_name = os.path.basename(file)
-            cutil.write_file('preprocessed_' + file_name, x, y)
